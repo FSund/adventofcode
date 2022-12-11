07ec0acf26cc869c154e86b10e46957b106edbdb
Filip Sund <filip.sund@gmail.com>
2022-12-11 21:04:33 +0100

    wat**2

--- 11/run.py
+++ 11/run.py
@@ -1,8 +1,3 @@
-from math import floor
-from aoc import *
-
-# primes_to_keep = [2, 3, 5, 7, 11, 13, 17, 19, 23]
-
 lines = []
 with open("input.txt") as file:
     for line in file:
@@ -10,8 +5,6 @@ with open("input.txt") as file:
 
 
 class Monkey:
-    # primes_to_keep = [2, 3, 5, 7, 11, 13, 17, 19, 23]
-    # primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
     monkey_divisors = []
     monkey_factor = 1
     def __init__(self, starting_items, operation, test, true, false):
@@ -25,52 +18,25 @@ class Monkey:
         self.false_destination = int(false.strip("throw to monkey "))
         self.inspections = 0
     
-    def _find_factor(self, n):
-        factors = prime_factors(n)
-        
-        if n in factors:
-            factors.pop(factors.index(n))
-
-        for a in self.monkey_divisors:
-            if factors.count(a):
-                factors.pop(factors.index(a))
-        
-        factor = 1
-        for f in factors:
-            factor *= f
-
-        return factor
-    
     def inspect(self):
         for i in range(len(self.items)):
             old = self.items[i]
             self.items[i] = eval(self.operation)
-            # self.items[i] = floor(self.items[i] / 3.0)
-                    
+            
+            self.items[i] = self.items[i] % self.monkey_factor
+
             self.inspections += 1
 
     def test(self, monkeys):
         for i in reversed(range(len(self.items))):
-            # item = self.items[i]
             if self.items[i] % self.divisor == 0:
                 to = self.true_destination
             else:
                 to = self.false_destination
-            
-            # factors = get_repeated_prime_factors(self.items[i])
-            # factor = list_prod(factors)
-            # if not self.items[i] % factor == 0:
-            #     raise RuntimeError
-            # self.items[i] = self.items[i] // factor
-            
-            if self.items[i] > Monkey.monkey_factor**2:
-                factor = self._find_factor(self.items[i])
-                self.items[i] = self.items[i] // factor
-            
+
             if to == self.true_destination:
                 assert(self.items[i] % self.divisor == 0)
 
-            # to = int(op.strip("throw to monkey "))
             monkeys[to].items.append(self.items[i])
             self.items.pop(i)
 
@@ -98,7 +64,7 @@ print(f"{Monkey.monkey_factor=}")
 # Monkey 2 inspected items 8 times.
 # Monkey 3 inspected items 103 times.
 
-n = 20
+n = 10000
 print(f"n = {n}")
 for i in range(n):
     # print(monkeys[0].items)
@@ -116,4 +82,4 @@ print(f"max(items): {max(items)}")
 inspections = [monkey.inspections for monkey in monkeys]
 print(f"inspections: {inspections}")
 inspections.sort()
-print(inspections[-1] * inspections[-2])
+print(inspections[-1] * inspections[-2])  # 25590400731

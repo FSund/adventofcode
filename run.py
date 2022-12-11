c51f4ec0cb0dbaf69f3dd3746830a05607250b2e
Filip Sund <filip.sund@gmail.com>
2022-12-11 17:21:14 +0100

    Revert "Prime factor stuff that doesn't work"
    This reverts commit ee636bb8b40b2f8d03dbec842816da06a6e2df27.

--- run.py
+++ run.py
@@ -5,98 +5,26 @@ with open("input.txt") as file:
     for line in file:
         lines.append(line.strip("\n"))
 
-# class :
-
-# class Factors:
-#     def __init__(self, factors=None):
-#         if factors:
-#             self.factors = factors
-#         else:
-#             self.factors = {}
-#             for f in [1, 2, 3, 5, 7, 11, 13, 17, 19]:
-#                 self.factors[f] = 0
-    
-#     def add(self, value):
-#         self.factors[1] += value
-    
-#     def multiply(self, value):
-#         self.factors[value] += 1
-#         self.factors[1] *= 
-    
-#     def multiply_with_factors(self, factors):
-#         for key in self.factors.keys():
-#             self.factors[key] += factors[key]
-
-class Product:
-    primes = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23]
-    max_prime = max(primes)
-    def __init__(self, value):
-        # 1, 2, 3, 5, 7, 11, 13, 17, 19, and 23
-        self.factors = [0 for i in range(self.max_prime+1)]
-        self.factors[1] = value
-    
-    def multiply(self, number):
-        self.factors[number] += 1
-    
-    def _get_value(self):
-        value = 1
-        for prime in self.primes:
-            if self.factors[prime]:
-                value *= prime*self.factors[prime]
-        return value
-    
-    def is_divisible_by(self, number):
-        if self.factors[number] > 0:
-            return True
-        if self.factors[1] % number:
-            return True
-        return False
-
-a = Product(79)
-print(a._get_value())
-a.multiply(2)
-print(a._get_value())
-a.multiply(5)
-print(a._get_value())
-a.multiply(2)
-print(a._get_value())
-a.multiply(23)
-print(a._get_value())
-
-print(15*2*5*2*23)
-print(a.is_divisible_by(5))
-print(a.is_divisible_by(23))
-print(a.is_divisible_by(2))
-# print(a.is_divisible_by(79))
-
-
-class Item:        
+class Item:
     def __init__(self, value):
         # self.value = value
-        # self.factors = {}
-        # self.factors[1] = value
-        self.elements = [Product(value)]
+        self.factors = {}
+        self.factors[1] = value
     
-    # def multiply_self(self):
-    #     self.factors[1] *= 2
+    def multiply_self(self):
+        self.factors[1] *= 2
     
-    def multiply(self, number):
-        for i in range(len(self.elements)):
-            self.elements[i].multiply(number)
-            
-    def add(self, number):
-        self.elements.append(Product(number))
-    
-    def square(self):
-        # self.elements += self.elements
-        pass
+    def add_factor(self, factor):
+        if factor not in self.factors:
+            self.factors[factor] = 0
+        self.factors[factor] += 1
 
     def is_divisible_by(self, number):
-        for element in self.elements:
-            if not element.is_divisible_by(number):
-                return False
-        return True
-
+        if self.factors[1] % number == 0:
+            return True
+        else:
+            return False
+            
 
 class Monkey:
     def __init__(self, starting_items, operation, test, true, false):
@@ -112,12 +40,12 @@ class Monkey:
             if "*" in self.operation:
                 factor = self.operation.split(" * ")[1]
                 if factor == "old":
-                    self.items[i].square()
+                    self.items[i].multiply_self()
                 else:
-                    self.items[i].multiply(int(factor))
+                    self.items[i].add_factor(int(factor))
             else:
                 value = int(self.operation.split(" + ")[1])
-                self.items[i].add(value)
+                self.items[i].factors[1] += value
 
             # self.items[i] = floor(self.items[i] / 3.0)  # ignore for star 2
             
@@ -163,29 +91,29 @@ while i < len(lines):
 
 # divide worry level by 3 (and round down to nearest int) after inspection (before test)
 
-# def prime_factors(n):
-#     i = 2
-#     factors = []
-#     while i * i <= n:
-#         if n % i:
-#             i += 1
-#         else:
-#             n //= i
-#             factors.append(i)
-#     if n > 1:
-#         factors.append(n)
-#     return factors
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
 
 # mult = 1
 # for monkey in monkeys:
 #     mult *= monkey._test
 
-for i in range(100):
+for i in range(20):
     for monkey in monkeys:
         monkey.inspect()
         monkey.test(monkeys)
 
-print(len(monkeys[0].items))
+print(monkeys[0].items[0].factors)
 
 # items = []
 # for monkey in monkeys:

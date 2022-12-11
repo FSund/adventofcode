59320ce1d9d07d68e59a913146030c21b18556fc
Filip Sund <filip.sund@gmail.com>
2022-12-11 19:25:55 +0100

    Works, but too slow

--- run.py
+++ run.py
@@ -1,37 +1,69 @@
 from math import floor
 
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
 lines = []
 with open("input.txt") as file:
     for line in file:
         lines.append(line.strip("\n"))
 
 class Monkey:
+    # primes = [2, 3, 5, 7, 11, 13, 17, 19, 23]
+    primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
     def __init__(self, starting_items, operation, test, true, false):
-        self.items = starting_items[::-1]  # REVERSE
-        self.operation = operation
-        self._test = test
-        self.true = true
-        self.false = false
+        self.items = starting_items
+        self.operation = operation.split("=")[1]
+        self.divisor = int(test.strip("divisible by "))
+        self.true_destination = int(true.strip("throw to monkey "))
+        self.false_destination = int(false.strip("throw to monkey "))
         self.inspections = 0
     
     def inspect(self):
         for i in range(len(self.items)):
             old = self.items[i]
-            self.items[i] = eval(self.operation.split("=")[1])
-            self.items[i] = floor(self.items[i] / 3.0)
+            self.items[i] = eval(self.operation)
+            # self.items[i] = floor(self.items[i] / 3.0)
+                    
             self.inspections += 1
 
     def test(self, monkeys):
-        for i in list(range(len(self.items)))[::-1]: # REVERSE
-            test = int(self._test.strip("divisible by "))
-            item = self.items[i]
-            if item % test == 0:
-                op = self.true
+        for i in reversed(range(len(self.items))):
+            # item = self.items[i]
+            if self.items[i] % self.divisor == 0:
+                to = self.true_destination
             else:
-                op = self.false
+                to = self.false_destination
+            
+            # print(self.items[i])
+            # reduce angst
+            for prime in self.primes:
+                if self.items[i] < prime:
+                    break
 
-            to = int(op.strip("throw to monkey "))
-            monkeys[to].items.append(item)
+                quotient, remainder = divmod(self.items[i], prime)
+                while remainder == 0 and remainder > 1:
+                    self.items[i] = quotient
+                    quotient, remainder = divmod(self.items[i], prime)
+                
+                # quotient = number // divisor
+                # quotient, remainder = divmod(self.items[i], prime)
+                # while remainder == 0:
+                #     self.items[i] = quotient
+                #     quotient, remainder = divmod(self.items[i], prime)
+
+            # to = int(op.strip("throw to monkey "))
+            monkeys[to].items.append(self.items[i])
             self.items.pop(i)
 
 monkeys = []
@@ -49,14 +81,22 @@ while i < len(lines):
     
     i += 7
 
-# divide worry level by 3 (and round down to nearest int) after inspection (before test)
-
-for i in range(20):
+n = 20
+print(f"n = {n}")
+for i in range(n):
+    # print(monkeys[0].items)
     for monkey in monkeys:
         monkey.inspect()
         monkey.test(monkeys)
 
+items = []
+for monkey in monkeys:
+    items += monkey.items
+
+print(f"items: {items}")
+print(f"max(items): {max(items)}")
+
 inspections = [monkey.inspections for monkey in monkeys]
+print(f"inspections: {inspections}")
 inspections.sort()
-print(inspections)
 print(inspections[-1] * inspections[-2])

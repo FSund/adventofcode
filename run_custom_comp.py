a1e5ddf4f4ac1fe5f578bba8afa2d476079559cf
Filip Sund <filip.sund@bouvet.no>
2022-12-13 18:25:40 +0100

    WORKS

--- run_custom_comp.py
+++ run_custom_comp.py
@@ -22,16 +22,20 @@ class Packet:
             return self < other
         
         if isinstance(self.value, list) and isinstance(other.value, list):
+            # print("case list list")
             for left, right in zip(self.value, other.value):
                 left = Packet(left) 
                 right = Packet(right)
                 if left == right:
                     continue
-                return left < right
+                else:
+                    return left < right
+            
+            # If the left list runs out of items first, the inputs are in the right order.
             if len(self.value) < len(other.value):
-                return True  # There were more items in the second tuple
+                return True
             else:
-                return False  # The first tuple had more items or they are equal
+                return False
         
         raise RuntimeError
 
@@ -59,12 +63,18 @@ class Packet:
                     pass
                 else:
                     equal = False
-            return equal
+            if equal:
+                if len(self.value) == len(other.value):
+                    return True
+                else:
+                    return False
+            else:
+                return False
         
         raise RuntimeError
-        
 
-if __name__ == "__main__":
+
+def star1():
     lines = []
     with open("input.txt") as file:
         for line in file:
@@ -84,9 +94,9 @@ if __name__ == "__main__":
         p1 = Packet(p1)
         p2 = Packet(p2)
         
-        result = p1 > p2
+        result = p1 < p2
         if result:
-            # print(f"right order: {idx} ({lines[p1_idx]} and {lines[p2_idx]})")
+            print(f"right order: {idx} ({lines[p1_idx]} and {lines[p2_idx]})")
             right_order.append(idx)
     
         idx += 1
@@ -95,9 +105,17 @@ if __name__ == "__main__":
     
     print(f"first star: {sum(right_order)}")
     # 5190 too low
+
+
+def star2():
+    lines = []
+    with open("input.txt") as file:
+        for line in file:
+            lines.append(line.strip("\n"))
     
     lines.append("[[2]]")
     lines.append("[[6]]")
+    
     packets = []
     for line in lines:
         if line == "":
@@ -105,5 +123,33 @@ if __name__ == "__main__":
         p = Packet(eval(line))
         packets.append(p)
     packets.sort()
-    for p in packets:
-        print(p)
+    # for p in packets:
+    #     print(p)
+    
+    i1 = 0
+    i2 = 0
+    for idx, p in enumerate(packets):
+        if p.value == [[2]]:
+            i1 = idx+1
+        if p.value == [[6]]:
+            i2 = idx+1
+    print(f"star 2: {i1*i2}")
+
+
+def testing():
+    p1 = Packet([[[]]])
+    p2 = Packet([])
+    assert(p2 < p1)
+    assert(p2 != p1)
+    assert(not Packet([1]) < Packet([1]))
+    assert(Packet([1]) < Packet([2]))
+    print(Packet([1,1,3,1,1]) < Packet([1,1,5,1,1]))
+
+
+if __name__ == "__main__":
+    # Your puzzle answer was 5340.
+    # star1()
+    
+    # testing()
+    
+    star2()

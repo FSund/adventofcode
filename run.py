3554f258a7f5973d55a916b2bf6977cbbf02b728
Filip Sund <filip.sund@bouvet.no>
2022-12-14 12:13:42 +0100

    actuall parse input

--- run.py
+++ run.py
@@ -23,8 +23,8 @@ def find_boundaries(lines):
         coords = line.split(" -> ")
         for pos in coords:
             x, y = (int(c) for c in pos.split(","))
-            print(x)
-            print(y)
+            # print(x)
+            # print(y)
             if x > x_max:
                 x_max = x
             if x < x_min:
@@ -37,6 +37,10 @@ def find_boundaries(lines):
     return x_min, x_max, y_min, y_max
 
 
+# def step(frame, source=[500, 0]):
+    
+
+
 if __name__ == "__main__":
 
     lines = []
@@ -47,10 +51,10 @@ if __name__ == "__main__":
     x_min, x_max, y_min, y_max = find_boundaries(lines)
     x_min -= 1
     x_max += 1
-    # y_min -= 1
-    # y_max += 1
-    y_min = 0
-    y_max = 9
+    y_min -= 1
+    y_max += 1
+    # y_min = 0
+    # y_max = 9
     
     # use full frame, only print part of it
     x_min = 0
@@ -69,8 +73,38 @@ if __name__ == "__main__":
     # source of sand
     frame[0][500] = "+"
     
+    # set up walls
+    for line in lines:
+        coords = line.split(" -> ")
+        for idx in range(len(coords) - 1):
+            i0 = int(coords[idx].split(",")[1])  # use index 1 for i and 0 for j
+            j0 = int(coords[idx].split(",")[0])
+            i1 = int(coords[idx+1].split(",")[1])
+            j1 = int(coords[idx+1].split(",")[0])
+            
+            if i1 < i0:
+                # print(f"{i0} -> {i1}")
+                i0, i1 = i1, i0
+                # print(f"{i0} -> {i1}\n")
+            if j1 < j0:
+                # print(f"{j0} -> {j1}")
+                j0, j1 = j1, j0
+                # print(f"{j0} -> {j1}\n")
+
+            # print(f"{i0}, {j0} -> {i1}, {j1}")
+            if i0 == i1:
+                i = i0
+                for j in range(j0, j1+1):
+                    frame[i][j] = "#"
+            if j0 == j1:
+                j = j0
+                for i in range(i0, i1+1):
+                    frame[i][j] = "#"
+    
     
     for i in range(len(frame)):
         row = "".join([frame[i][j] for j in range(494, 504)])
         
         print(f"{i} {row}")
+        
+    

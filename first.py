7a5cb9701eff976d7e0b5f3a26375bca7ffd883f
Filip Sund <filip.sund@bouvet.no>
2022-12-19 08:22:02 +0100

    plotting

--- first.py
+++ first.py
@@ -1,5 +1,8 @@
+import cc3d
 import numpy as np
-import itertools
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 def get_input(filename="example.txt"):
     lines = []
@@ -8,372 +11,92 @@ def get_input(filename="example.txt"):
             lines.append(line.strip("\n"))
     return lines
 
-# ####
 
-# .#.
-# ###
-# .#.
-
-# ..#
-# ..#
-# ###
-
-# #
-# #
-# #
-# #
-
-# ##
-# ##
-
-# make rock types
-horiz = np.zeros((1, 4), dtype=int)
-horiz.fill(2)
-star = np.zeros((3, 3), dtype=int)
-star[0,1] = 2
-star[1,0] = 2
-star[1,1] = 2
-star[1,2] = 2
-star[2,1] = 2
-corner = np.zeros((3, 3), dtype=int)
-corner[0,2] = 2
-corner[1,2] = 2
-corner[2,2] = 2
-corner[2,0] = 2
-corner[2,1] = 2
-vert = np.zeros((4, 1), dtype=int)
-vert.fill(2)
-square = np.zeros((2, 2), dtype=int)
-square.fill(2)
-
-rocks = [horiz, star, corner, vert, square]
+def get_max_index(lines):
+    max_xyz = [0,0,0]
+    for line in lines:
+        xyz = eval(line)
+        for i in range(3):
+            if xyz[i] > max_xyz[i]:
+                max_xyz[i] = xyz[i]
+    return max(max_xyz)
 
 
-def find_top_rock_idx(frame):
-    i = np.argmax(
-        np.sum(frame > 1, axis=1) > 0
+def get_scan(lines, n):
+    scan = np.zeros((n,n,n), dtype=bool)
+    for line in lines:
+        i, j, k = eval(line)
+        scan[i, j, k] = 1
+    return scan
+
+
+def find_area_bfs(voxels, start):
+    visited = set() # to keep track of already visited nodes
+    bfs_traversal = list()  # the BFS traversal result
+    queue = list()  # queue
+    parent = {}  # dict
+    
+    # push the root node to the queue and mark it as visited
+    queue.append(start)
+    visited.add(start)
+    
+    adjacent = (
+        (0, 0, -1), (0, 0, +1), 
+        (0, -1, 0), (0, +1, 0), 
+        (-1, 0, 0), (+1, 0, 0),
     )
-    return i
-
-
-def find_spawn_idx(frame, rock):
-    # rock "position" is top left pixel of rock
-    i = find_top_rock_idx(frame)
     
-    # print(f"top rock row idx: {i}")
-    # print(frame[i-10:])
-    # a = frame>1
-    # print(a[i-10:])
-    # b = np.sum(frame > 1, axis=1)
-    # print(b[i-10:])
-    # c = b > 0
-    # print(c[i-10:])
-    
-    
-    return i - 3  - rock.shape[0]
-
-
-def frame_to_example_ascii(frame, irange=None):
-    lines = []
-    if irange is None:
-        irange = range(frame.shape[0])
-    for i in irange:
-        line = ""
-        for j in range(frame.shape[1]):
-            if frame[i,j] == 0:  # free
-                line += "."
-            elif frame[i,j] == 2:  # moving rock
-                line += "@"
-            elif frame[i,j] == 4:  # stopped rock
-                line += "#"
-            elif frame[i,j] == 1:  # boundaries
-                if i == frame.shape[0]-1:
-                    if j == 0 or j == frame.shape[1]-1:
-                        line += "+"
-                    else:
-                        line += "-"
-                else:
-                    line += "|"
-        lines.append(line)
+    # loop until the queue is empty
+    while queue:
+        # pop the front node of the queue and add it to bfs_traversal
+        current_pos = queue.pop(0)
+        bfs_traversal.append(current_pos)
         
-    return lines
+        # check all the neighbour nodes of the current node
+        for translation in adjacent:
+            pos = (current_pos[0] + translation[0], current_pos[1] + translation[1])
+            
+            # Make sure within range
+            if (pos[0] > (voxels.shape[0] - 1) or pos[0] < 0 
+                or pos[1] > (voxels.shape[1] - 1) or pos[1] < 0
+                or pos[2] > (voxels.shape[2] - 1) or pos[2] < 0 
+            ):
+                continue
+
+            # if the neighbour nodes are not already visited, 
+            # push them to the queue and mark them as visited
+            if pos not in visited:
+                visited.add(pos)
+                queue.append(pos)
+                parent[pos] = current_pos
+
+    return bfs_traversal
 
 
-def compare_frame_to_example_ascii(frame):
-    lines_ex = []
-    with open("example_ascii_final.txt") as f:
-        for line in f:
-            lines_ex.append(line.strip("\n"))
+def plot_3d(labels):
+    # set the colors of each object
+    N = np.max(labels)
+    norm = mpl.colors.Normalize(vmin=0, vmax=N+1)
+    cmap = cm.hot
+    m = cm.ScalarMappable(norm=norm, cmap=cmap)
     
-    lines = frame_to_example_ascii(frame, irange=range(frame.shape[0]-22, frame.shape[0]))
-    for l0, l1 in zip(lines_ex, lines):
-        assert(l0 == l1)
-        if l0 != l1:
-            raise RuntimeError("example not equal")
-
-
-def print_full_frame(frame, irange=None):
-    if irange is None:
-        irange = range(frame.shape[0])
-    for i in irange:
-        line = f"{i:3d} "
-        for j in range(frame.shape[1]):
-            if frame[i,j] == 0:  # free
-                line += "."
-            elif frame[i,j] == 2:  # moving rock
-                line += "@"
-            elif frame[i,j] == 4:  # stopped rock
-                line += "#"
-            elif frame[i,j] == 1:  # boundaries
-                if i == frame.shape[0]-1:
-                    if j == 0 or j == frame.shape[1]-1:
-                        line += "+"
-                    else:
-                        line += "—"
-                else:
-                    line += "|"
-        print(line)
-
-
-def print_part_frame_from_rock(frame, rock_pos):
-    # print(np.sum(frame[0:-2, 1:-2], axis=1))
-    # top_rock_idx = find_top_rock_idx(frame)
-    # print(frame[41])
-    # print(top_rock_idx-2)
-    for i in range(rock_pos[0]-1, frame.shape[0]):
-        line = f"{i:3d} "
-        for j in range(frame.shape[1]):
-            if frame[i,j] == 0:  # free
-                line += "."
-            elif frame[i,j] == 2:  # moving rock
-                line += "@"
-            elif frame[i,j] == 4:  # stopped rock
-                line += "#"
-            elif frame[i,j] == 1:  # boundaries
-                if i == frame.shape[0]-1:
-                    if j == 0 or j == frame.shape[1]-1:
-                        line += "+"
-                    else:
-                        line += "—"
-                else:
-                    line += "|"
-        print(line)
-
-
-
-# The tall, vertical chamber is exactly seven units wide. 
-
-# Each rock appears so that its left edge is two units away from the left wall
-# and its bottom edge is three units above the highest rock in the room 
-# (or the floor, if there isn't one).
-
-
-def add_rock(frame, rock, position):
-    # "position" is top left corner of rock
-    i0 = position[0]
-    i1 = i0 + rock.shape[0]
-    j0 = position[1]
-    j1 = j0 + rock.shape[1]
-
-    frame[i0:i1, j0:j1] += rock
-
-
-def remove_rock(frame, rock, position):
-    # "position" is top left corner of rock
-    i0 = position[0]
-    i1 = i0 + rock.shape[0]
-    j0 = position[1]
-    j1 = j0 + rock.shape[1]
-
-    frame[i0:i1, j0:j1] -= rock
-
-
-def spawn_rock(frame, rock):
-    # rock_idx = rock_idx % (len(rocks) - 1)
-    position = [0, 0]
-    position[0] = find_spawn_idx(frame, rock)
-    position[1] = 3
+    colors = np.empty(labels.shape + (4,))
+    for label in range(1, N+1):
+        colors[labels==label] = m.to_rgba(label)
     
-    add_rock(frame, rock, position)
-
-    return position
-
-
-def _push(frame, rock, pos, dy):
-    # remove current rock
-    frame[pos[0]:pos[0] + rock.shape[0], pos[1]:pos[1] + rock.shape[1]] -= rock
-    
-    # new rock position
-    i0 = pos[0]
-    i1 = pos[0] + rock.shape[0]
-    j0 = pos[1] + dy
-    j1 = pos[1] + dy + rock.shape[1]
-    
-    # check if new position is free
-    if np.any(np.logical_and(frame[i0:i1, j0:j1], rock)):
-        success = False
-        # add back rock
-        frame[pos[0]:pos[0] + rock.shape[0], pos[1]:pos[1] + rock.shape[1]] += rock
-    else:
-        success = True
-        frame[i0:i1, j0:j1] += rock
-        pos[1] += dy
-    
-    return pos, success
-
-
-def push(frame, rock, pos, move):
-    # "<" means a push to the left, 
-    # while ">" means a push to the right
-    if move == "<":
-        dy = -1
-    elif move == ">":
-        dy = +1
-    
-    return _push(frame, rock, pos, dy)
-
-
-def fall(frame, rock, pos, dx=1):
-    # remove existing rock
-    frame[pos[0]:pos[0] + rock.shape[0], pos[1]:pos[1] + rock.shape[1]] -= rock
-
-    # new rock position
-    i0 = pos[0] + dx
-    i1 = pos[0] + dx + rock.shape[0]
-    j0 = pos[1]
-    j1 = pos[1] + rock.shape[1]
-
-    # check if new position is free    
-    if np.any(np.logical_and(frame[i0:i1, j0:j1], rock)):
-        success = False
-        # add fixed rock
-        frame[pos[0]:pos[0] + rock.shape[0], pos[1]:pos[1] + rock.shape[1]] += 2*rock
-    else:
-        success = True
-        frame[i0:i1, j0:j1] += rock
-        pos[0] += dx
-    
-    return pos, success
-
-def step(frame, rock, rocks_it, next_action, rock_position, moves_it, n_rocks):
-    if next_action == 0:
-        # spawn
-        rock = next(rocks_it)
-        position = spawn_rock(frame, rock)
-        next_action = 1
-        # print(f"spawn at {position}")
-        n_rocks += 1
-    elif next_action == 1:  # PUSH
-        # push if possible
-        move = next(moves_it)
-        position, success = push(frame, rock, rock_position, move)
-        # print(f"push {success}")
-        next_action = 2
-    elif next_action == 2:  # FALL
-        # fall, if possible
-        position, success = fall(frame, rock, rock_position)
-        # print(f"fall {success}")
-        if success:
-            # push
-            next_action = 1
-        else:
-            # spawn new
-            next_action = 0
-    
-    return position, next_action, rock, n_rocks
-
-
-# def test_moves(moves):
-#     move = next(moves)
-#     print(move)
-
-def check_example():
-    frame = np.zeros((5000, 9), dtype=int)
-    
-    # chamber limits
-    frame[-1,:] = 1
-    frame[:,0] = 1
-    frame[:,-1] = 1
-    
-    # spawn first rock manually
-    rock_position = [frame.shape[0] - 4 - rocks[0].shape[0], 3]
-    add_rock(frame, rocks[0], rock_position)
-    
-    lines = get_input("example.txt")
-    
-    moves = []
-    for c in lines[0]:
-        moves.append(c)
-
-    n_rocks = 1
-    next_action = 1  # 0: spawn, 1: push, 3: fall
-    moves_it = itertools.cycle(moves)
-    rocks_it = itertools.cycle(rocks)
-    rock = next(rocks_it)  # get first rock
-    n_rocks = 1
-    while n_rocks < 11:
-        rock_position, next_action, rock, n_rocks = step(frame, rock, rocks_it, next_action, rock_position, moves_it, n_rocks)
-
-    compare_frame_to_example_ascii(frame)
-    assert(np.all(frame <= 4))
-    
-    return True
-
-
-def star1(filename="example.txt"):
-    frame = np.zeros((5000, 9), dtype=int)
-    
-    # chamber limits
-    frame[-1,:] = 1
-    frame[:,0] = 1
-    frame[:,-1] = 1
-    
-    # print(rocks)
-    
-    # spawn first rock manually
-    rock_position = [frame.shape[0] - 4 - rocks[0].shape[0], 3]
-    add_rock(frame, rocks[0], rock_position)
-    # print(frame)
-    
-    # print_part_frame_from_rock(frame, rock_position)
-    
-    lines = get_input(filename)
-    
-    moves = []
-    for c in lines[0]:
-        moves.append(c)
-    # print(moves)
-    
-    # rock_idx = 1
-    n_rocks = 1
-    next_action = 1  # 0: spawn, 1: push, 3: fall
-    moves_it = itertools.cycle(moves)
-    rocks_it = itertools.cycle(rocks)
-    rock = next(rocks_it)  # get first rock
-    n_rocks = 1
-    while n_rocks < 2023:
-        rock_position, next_action, rock, n_rocks = step(frame, rock, rocks_it, next_action, rock_position, moves_it, n_rocks)
-        # print(rock_position)
-        # print(n_rocks)
-        # print_frame(frame, rock_position)
-    
-    # print_part_frame_from_rock(frame, rock_position)
-    
-    remove_rock(frame, rock, rock_position)
-    idx = find_top_rock_idx(frame)
-    # print(idx)
-    # print(f"example: {idx-1}")
-    print(f"star 1: {frame.shape[0] - idx - 1}")
-    
-    assert(np.all(frame <= 4))
-    
-    # print_full_frame(frame, range(idx-10, idx+10))
+    ax = plt.figure().add_subplot(projection='3d')
+    ax.voxels(labels, facecolors=colors)
 
 
 if __name__ == "__main__":
-    if not check_example():
-        print("TESTS FAILED")
-        raise RuntimeError("TESTS FAILED")
-
-    star1("example.txt")
-    star1("input.txt")
+    lines = get_input()
+    max_xyz = get_max_index(lines)
+    scan = get_scan(lines, max_xyz+1)
+    
+    labels, N = cc3d.connected_components(scan, connectivity=6,return_N=True)
+    print(f"{N = }")
+    print(np.max(labels))
+    
+    stats = cc3d.statistics(labels)
+    
+    plot_3d(labels)

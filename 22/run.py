632c522c07590205b35bb667dccb2e4139611233
Filip Sund <filip.sund@bouvet.no>
2022-12-22 17:33:46 +0100

    22 star 1 maybe

--- 22/run.py
+++ 22/run.py
@@ -1,6 +1,7 @@
 import numpy as np
 import re
 from math import copysign
+from enum import Enum
 
 def get_input(filename="input.txt"):
     lines = []
@@ -10,8 +11,48 @@ def get_input(filename="input.txt"):
     return lines
 
 
+class NodeType(Enum):
+    NONE = -1
+    FREE = 0
+    WALL = 1
+
+
+class MapNode:
+    def __init__(self, pos, type=NodeType.NONE):
+        self.type = type
+        self.pos = pos
+
+        # neighbors
+        self.right = None
+        self.left = None
+        self.up = None
+        self.down = None
+    
+    def __repr__(self):
+        rmap = {
+            "0": ">",
+            "1": "v",
+            "2": "<",
+            "3": "^",
+        }
+        return f"{self.type} [{self.pos[0]}, {self.pos[1]}]"
+        
+    def remove_none_connections(self, map):
+        while self.left.type == NodeType.NONE:
+            self.left = self.left.left
+        
+        while self.right.type == NodeType.NONE:
+            self.right = self.right.right
+        
+        while self.up.type == NodeType.NONE:
+            self.up = self.up.up
+        
+        while self.down.type == NodeType.NONE:
+            self.down = self.down.down
+
+
 class Pos:
-    def __init__(self, pos, rot):
+    def __init__(self, pos: MapNode, rot):
         self.pos = pos
         # 0 for right (>), 1 for down (v), 2 for left (<), and 3 for up (^)
         self.rot = rot
@@ -23,7 +64,7 @@ class Pos:
             "2": "<",
             "3": "^",
         }
-        return f"[{self.pos[0]}, {self.pos[1]}] {self.rot}"
+        return f"[{self.pos.pos[0]}, {self.pos.pos[1]}] {self.rot}"
 
     def rotate(self, turn):
         if turn == "R":
@@ -34,47 +75,43 @@ class Pos:
             raise RuntimeError
 
         self.rot = self.rot % 4
-    
-    def _move_horiz(self, map, d):
-        print(f"moving {d} horizontally from {self.pos}")
+
+    def _move_left(self, distance):
+        for i in range(distance):
+            self.pos = self.pos.left
         
-        i = self.pos[0]
-        one = int(copysign(d, 1))
-        for d in range(d):
-            i += one
-            # check boundaries
-        self.pos[0] = i
+    def _move_right(self, distance):
+        for i in range(distance):
+            self.pos = self.pos.right
     
-    def _move_vert(self, map, d):
-        print(f"moving {d} vertically from {self.pos}")
-        
-        j = self.pos[1]
-        one = int(copysign(d, 1))
-        for d in range(d):
-            j += one
-            # check boundaries
-        self.pos[1] = j
+    def _move_up(self, distance):
+        for i in range(distance):
+            self.pos = self.pos.up
     
-    def move(self, map, d):
+    def _move_down(self, distance):
+        for i in range(distance):
+            self.pos = self.pos.down
+    
+    def move(self, d):
         if self.rot == 0:  # right
-            self._move_horiz(map, d)
+            self._move_right(d)
         elif self.rot == 1:  # down
-            self._move_vert(map, d)
+            self._move_down(d)
         elif self.rot == 2:  # left
-            self._move_horiz(map, -d)
+            self._move_left(d)
         elif self.rot == 3:  # up
-            self._move_vert(map, -d)
+            self._move_up(d)
         else:
             raise RuntimeError
 
-    def do_operation(self, map, operation):
+    def do_operation(self, operation):
         if operation.isdigit():
-            self.move(map, int(operation))
+            self.move(int(operation))
         else:
             self.rotate(operation)
 
 
-def make_map(lines):
+def make_map_nodes(lines):
     width = 0
     height = 0
     for line in lines:
@@ -85,38 +122,86 @@ def make_map(lines):
             width = len(line)
     
     cmap = {
-        " ": -1,
-        ".": 0,
-        "#": 1,
+        " ": NodeType.NONE,
+        ".": NodeType.FREE,
+        "#": NodeType.WALL,
     }
-    map = np.empty((height, width), dtype=int)
-    map.fill(-1)
+    map = []
     for i in range(height):
         line = lines[i]
+        map.append([])
         for j in range(len(line)):
             p = line[j]
-            map[i,j] = cmap[p]
+            map[i].append(
+                MapNode(
+                    pos=(i, j),
+                    type=cmap[p]
+                )
+            )
+    
+    
+    # if i > 0 and i < (height-1) and j > 0 and j < (width - 1):
+    #     map[i][j].left = map[i-1]
+        
+    m = len(map)
+    n = len(map[0])
+    
+    # connect nodes
+    for i in range(m):
+        for j in range(n):
+            current = map[i][j]
+            
+            if i == 0:  # top row
+                current.up = map[m-1][j]
+                current.down = map[i+1][j]  # default
+            elif i == (m-1):  # bottom row
+                current.up = map[i-1][j]  # default
+                current.down = map[0][j]
+            else:
+                current.up = map[i-1][j]
+                current.down = map[i+1][j]
+
+            if j == 0:  # leftmost column
+                current.left = map[i][n-1]
+                current.right = map[i][j+1]  # default
+            elif j == (n-1):  # rightmost column
+                current.left = map[i][j-1]  # default
+                current.right = map[i][0]
+            else:
+                current.left = map[i][j-1]
+                current.right = map[i][j+1]
+
+    # remove NONE
+    for i in range(m):
+        for j in range(n):
+            current = map[i][j]
+            current.remove_none_connections(map)
     
     return map
 
 
-def star1(filename):
+def star1_v2(filename):
     lines = get_input(filename)
-    map = make_map(lines)
+    map = make_map_nodes(lines)
     
-    i = 0
-    j = np.argmax(map[i,:] == 0)
-    pos = Pos([i, j], 0)
+    # find start
+    current = None
+    for j in range(len(map[0])):
+        if map[0][j].type != NodeType.NONE:
+            current = map[0][j]
+            break
+
+    print(current)
+    
+    pos = Pos(current, 0)
     operations = re.split('([R,L])', lines[-1])
     for op in operations:
-        pos.do_operation(map, op)
+        pos.do_operation(op)
         print(pos)
-    
-    return 6032
 
 
 if __name__ == "__main__":
-    assert(star1("22/example.txt") == 6032)
+    assert(star1_v2("22/example.txt") == 6032)
     # assert(star1("22/input.txt") == 169525884255464)
     # print(f'star 1: {star1("22/input.txt")}')
 

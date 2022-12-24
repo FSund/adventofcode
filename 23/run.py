54df58a4f2b8d8e36d3cdd92b94d0f5b4d4c74b8
Filip Sund <filip.sund@gmail.com>
2022-12-24 09:21:11 +0100

    star 1 example

--- 23/run.py
+++ 23/run.py
@@ -11,7 +11,7 @@ def get_input(filename="input.txt"):
 # def print_elves(elves):
 #     for i in range(len(elves)):
 
-neighbors = [
+all_neighbors = [
     [-1, -1], [-1, 0], [-1, 1],
     [ 0, -1],          [ 0, 1],
     [ 1, -1], [ 1, 0], [1,  1],
@@ -34,6 +34,16 @@ class Elf:
     
     def propose_move(self, map, suggestions):
         self.suggestion = None
+
+        # check total neighbors first
+        total_neighbors = 0
+        for neighbor in all_neighbors:
+            i = self.pos[0] + neighbor[0]
+            j = self.pos[1] + neighbor[1]
+            total_neighbors += map[i,j]
+        if total_neighbors == 0:
+            return
+            
         for dx in range(4):  # loop over 4 suggest directions
             idx = (propose_idx + dx) % 4
             
@@ -88,29 +98,53 @@ def solve(filename):
     for i in range(m):
         for j in range(n):
             if lines[i][j] == "#":
-                map[i+offset[0],j+offset[1]] += 1
-                elves.append(Elf((i+offset[0], j+offset[1])))
+                ii = i+offset[0]
+                jj = j+offset[1]
+                map[ii,jj] += 1
+                elves.append(Elf((ii, jj)))
     
     suggestions = np.zeros((m + 2*offset[0], n + 2*offset[1]), dtype=int)
     
     # print_map(map)
-    print_map(map[5:10,5:10])
-    print()
+    # print_map(map)
+    # print()
     
     global propose_idx
-    for i in range(2):
+    for i in range(10):
         for elf in elves:
             elf.propose_move(map, suggestions)
         for elf in elves:
             elf.do_move(map, suggestions)
         suggestions.fill(0)
         propose_idx += 1
-        print_map(map[5:10,5:10])
-        print()
-        
+        # print_map(map)
+        # print()
     
     # print_map(map)
-
+    
+    i0 = 1e9
+    i1 = 0
+    j0 = 1e9
+    j1 = 0
+    for elf in elves:
+        if elf.pos[0] > i1:
+            i1 = elf.pos[0]
+        if elf.pos[0] < i0:
+            i0 = elf.pos[0]
+        
+        if elf.pos[1] > j1:
+            j1 = elf.pos[1]
+        if elf.pos[1] < j0:
+            j0 = elf.pos[1]
+    
+    # print_map(map[i0:i1+1, j0:j1+1])
+    # print(f"{i0 = }")
+    # print(f"{i1 = }")
+    # print(f"{i1-i0}")
+    # print(f"{j0 = }")
+    # print(f"{j1 = }")
+    # print(f"{j1-j0}")
+    return (i1-i0+1)*(j1-j0+1) - len(elves)
 
 
 example_round10 = [
@@ -130,4 +164,4 @@ example_round10 = [
 
 
 if __name__ == "__main__":
-    solve("23/example.txt")
+    print(f'star 1: {solve("23/example.txt")}')

c41f8cdd698d24eb5b9a12c7b7396527b7f4ea74
Filip Sund <filip.sund@gmail.com>
2023-12-17 15:40:10 +0100

    star 2

--- 2023/17/run.py
+++ 2023/17/run.py
@@ -15,36 +15,6 @@ def get_input(filename):
 
     return lines
 
-def test_bfs():
-    graph = {
-        '5' : ['3','7'],
-        '3' : ['2', '4'],
-        '7' : ['8'],
-        '2' : [],
-        '4' : ['8'],
-        '8' : []
-    }
-
-    visited = [] # List for visited nodes.
-    queue = []     #Initialize a queue
-
-    def bfs(visited, graph, node): #function for BFS
-        visited.append(node)
-        queue.append(node)
-
-        while queue:          # Creating loop to visit each node
-            m = queue.pop(0) 
-            print (m, end = " ") 
-
-            for neighbour in graph[m]:
-                if neighbour not in visited:
-                    visited.append(neighbour)
-                    queue.append(neighbour)
-
-        # Driver Code
-        print("Following is the Breadth-First Search")
-        bfs(visited, graph, '5')    # function calling
-
 def visualize_path(grid, path):
     for row in range(len(grid)):
         line = []
@@ -92,6 +62,121 @@ def shortest_path_with_constraints(grid, target=None):
                     heapq.heappush(queue, (new_cost, (new_row, new_col, new_direction, new_consecutive_moves), new_path))
 
 
+def ultra_crucible(grid, target=None):
+    rows, cols = len(grid), len(grid[0])
+    # start = (0, 0, 0, 0)  # (row, col, direction, consecutive_moves)
+    # queue = [(0, start, ())]  # (total cost, current node, current path)
+    
+    queue = [
+        # (total cost, current node, current path)
+        (0, (0,0,0,0), ()),  # right
+        (0, (0,0,1,0), ()),  # down
+    ]
+    visited = set()
+    max_consecutive_moves = 10
+    min_consecutive_moves = 4
+    
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # Right, Down, Left, Up
+    if target is None:
+        target = (rows - 1, cols - 1)
+
+    while queue:
+        cost, (row, col, direction, consecutive_moves), path = heapq.heappop(queue)
+        # print(f"{row = }, {col = }, {path = }")
+        # print(f"{consecutive_moves = }, {path = }")
+
+        if (row, col, direction, consecutive_moves) in visited:
+            # if (row, col, path) in visited:
+            continue
+        
+        visited.add((row, col, direction, consecutive_moves))
+        # visited.add((row, col, path))
+
+        if row == target[0] and col == target[1]:
+            return cost, path + ((row, col),)
+        
+        # need to move a minimum of 4 times in the same direction before it can turn
+        # can to move a maximum of 10 times in the same direction before it must turn
+        for i in range(len(directions)):
+            new_row = row + directions[i][0]
+            new_col = col + directions[i][1]
+            new_direction = i
+
+            if 0 <= new_row < rows and 0 <= new_col < cols and new_direction != (direction + 2) % 4:
+                new_cost = cost + grid[new_row][new_col]
+                new_consecutive_moves = consecutive_moves + 1 if direction == new_direction else 1
+                
+                new_path = path + ((row, col),)
+                
+                # can only change direction if consecutive moves is between min_consecutive_moves and max_consecutive_moves
+                if new_direction != direction:
+                    if min_consecutive_moves <= consecutive_moves <= max_consecutive_moves:
+                        heapq.heappush(queue, (new_cost, (new_row, new_col, new_direction, new_consecutive_moves), new_path))
+                else:  # new_direction == direction
+                    if new_consecutive_moves <= max_consecutive_moves:
+                        heapq.heappush(queue, (new_cost, (new_row, new_col, new_direction, new_consecutive_moves), new_path))
+                continue
+                
+                # if new_consecutive_moves == 1:
+                #     # all moves are valid if we are at the start of a new direction
+                #     heapq.heappush(queue, (new_cost, (new_row, new_col, new_direction, new_consecutive_moves), new_path))
+                if new_consecutive_moves < min_consecutive_moves:
+                    # can only continue straight if we are below the min consecutive moves
+                    if new_direction == direction:
+                        heapq.heappush(queue, (new_cost, (new_row, new_col, new_direction, new_consecutive_moves), new_path))
+                
+                elif new_consecutive_moves <= max_consecutive_moves:
+                    # can turn or continue if we are between the min and max consecutive moves
+                    heapq.heappush(queue, (new_cost, (new_row, new_col, new_direction, new_consecutive_moves), new_path))
+                else:
+                    # can only turn if we are above the max consecutive moves
+                    if new_direction != direction:
+                        heapq.heappush(queue, (new_cost, (new_row, new_col, new_direction, new_consecutive_moves), new_path))
+                
+                # if min_consecutive_moves < new_consecutive_moves <= max_consecutive_moves:
+                #     new_path = path + [(row, col)]  # Append current position to the path
+                #     heapq.heappush(queue, (new_cost, (new_row, new_col, new_direction, new_consecutive_moves), new_path))
+
+# def ultra2(grid, target=None):
+#     rows, cols = len(grid), len(grid[0])
+#     start = (0, 0, 0, 0)  # (row, col, direction, consecutive_moves)
+#     queue = [(0, start, [])]  # (total cost, current node, current path)
+#     visited = set()
+    
+#     directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # Right, Down, Left, Up
+#     if target is None:
+#         target = (rows - 1, cols - 1)
+
+#     while queue:
+#         cost, (row, col, direction, consecutive_moves), path = heapq.heappop(queue)
+
+#         if (row, col, direction, consecutive_moves) in visited:
+#             continue
+        
+#         visited.add((row, col, direction, consecutive_moves))
+
+#         if row == target[0] and col == target[1]:
+#             return cost, path + [(row, col)]
+        
+#         for i in range(len(directions)):
+#             new_row = row + directions[i][0]
+#             new_col = col + directions[i][1]
+#             new_direction = i
+
+#             if 0 <= new_row < rows and 0 <= new_col < cols and new_direction != (direction + 2) % 4:
+#                 if consecutive_moves < 10:  # Check if we haven't moved 10 steps in the same direction
+#                     new_cost = cost + grid[new_row][new_col]
+#                     new_consecutive_moves = consecutive_moves + 1 if direction == new_direction else 1
+                    
+#                     if new_consecutive_moves >= 4:  # Enforce at least 4 consecutive moves
+#                         new_path = path + [(row, col)]  # Append current position to the path
+#                         heapq.heappush(queue, (new_cost, (new_row, new_col, new_direction, new_consecutive_moves), new_path))
+#                 else:  # Forced to change direction after 10 steps
+#                     new_cost = cost + grid[new_row][new_col]
+#                     new_consecutive_moves = 1  # Reset consecutive moves to 1
+#                     new_path = path + [(row, col)]  # Append current position to the path
+#                     heapq.heappush(queue, (new_cost, (new_row, new_col, new_direction, new_consecutive_moves), new_path))
+
 def to_int(_lines):
     lines = []
     for line in _lines:
@@ -111,6 +196,18 @@ def star1(filename):
     #     print(node)
     return cost
 
+def star2(filename):
+    lines = get_input(filename)
+    lines = to_int(lines)
+    # print(lines)
+    
+    cost, path = ultra_crucible(lines)
+    cost2 = 0
+    for node in path[1:]:
+        cost2 += lines[node[0]][node[1]]
+    assert cost == cost2, f"wrong cost: {cost} != {cost2}"
+    return cost
+
 def tests():
     # grid = [
     #     [1, 2, 3, 4],
@@ -124,13 +221,13 @@ def tests():
     lines = to_int(lines)
     cost, path = shortest_path_with_constraints(lines, target=(0,8))  # (0, 8) fails
     # assert cost == 29, f"wrong cost: {cost}"
-    print(f"{cost = }")
-    visualize_path(lines, path)
+    # print(f"{cost = }")
+    # visualize_path(lines, path)
     # print(f"{path=}")
     cost = []
     for node in path[1:]:
         cost += [lines[node[0]][node[1]]]
-        print(f"{node}, cost: {cost}, total cost: {sum(cost)}")
+        # print(f"{node}, cost: {cost}, total cost: {sum(cost)}")
     
     # from example
     p = [
@@ -171,14 +268,36 @@ def tests():
         assert path[i] == p[i], f"wrong path: {path[i]} != {p[i]}"
         
     cost, path = shortest_path_with_constraints(lines)
-    visualize_path(lines, path)
-    for i in range(len(path)):
-        assert path[i] == p[i], f"wrong path at step {i}: {path[i]} != {p[i]}"
+    assert cost == 102, f"wrong cost: {cost}"
+    # visualize_path(lines, path)
+    # for i in range(len(path)):
+    #     assert path[i] == p[i], f"wrong path at step {i}: {path[i]} != {p[i]}"
     
+    # visualize_path(lines, path)
+    
+    
+    lines = get_input("example.txt")
+    lines = to_int(lines)
+    cost, path = ultra_crucible(lines)
     visualize_path(lines, path)
+    assert cost == 94, f"wrong cost: {cost}"
+    
+    lines = [
+        "111111111111", 
+        "999999999991", 
+        "999999999991", 
+        "999999999991", 
+        "999999999991", 
+    ]
+    lines = to_int(lines)
+    cost, path = ultra_crucible(lines)
+    print()
+    visualize_path(lines, path)
+    
+    # assert cost == 71, f"wrong cost: {cost}"
 
 if __name__ == "__main__":
-    # tests()
+    tests()
 
     ans = star1("example.txt")
     print(f"example star 1: {ans}")
@@ -186,15 +305,16 @@ if __name__ == "__main__":
     
     ans = star1("input.txt")
     print(f"star 1: {ans}")
+    assert ans == 1044
     # 1078 too high
     # 1077 too high
     
-    # ans = star2("example.txt")
-    # print(f"example star 2: {ans}")
-    # assert ans == 51
+    ans = star2("example.txt")
+    print(f"example star 2: {ans}")
+    assert ans == 94
     
-    # ans = star2("input.txt")
-    # print(f"star 2: {ans}")
+    ans = star2("input.txt")
+    print(f"star 2: {ans}")
     # # assert example == 145
     
     # 7792 too low

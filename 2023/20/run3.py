29f1e1789c0b31c7243005762b008625f73ae6af
Filip Sund <filip.sund@gmail.com>
2023-12-21 16:26:09 +0100

    star 2

--- 2023/20/run3.py
+++ 2023/20/run3.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from collections import OrderedDict
 import heapq
 from collections import deque
+from math import lcm
 
 HIGH = 1
 LOW = 0
@@ -71,10 +72,10 @@ class FlipFlop(Module):
 class Conjunction(Module):
     """
     Conjunction modules (prefix &) remember the type of the most recent pulse 
-    received from each of their connected input modules; they initially default 
-    to remembering a low pulse for each input. When a pulse is received, the 
-    conjunction module first updates its memory for that input. Then, if it 
-    remembers high pulses for all inputs, it sends a low pulse; otherwise, it 
+    received from each of their connected input modules; 
+    - they initially default to remembering a low pulse for each input. 
+    - When a pulse is received, the conjunction module first updates its memory for that input. 
+    - Then, if it remembers high pulses for all inputs, it sends a low pulse; otherwise, it 
     sends a high pulse.
     """
 
@@ -82,6 +83,7 @@ class Conjunction(Module):
         super().__init__()
         self.states = {}
         self.name = name
+        self.all_high = False
     
     def add_input(self, input):
         self.states[input] = LOW
@@ -91,9 +93,11 @@ class Conjunction(Module):
         self.states[source] = pulse
         calls = []
         if all(state == HIGH for state in self.states.values()):
+            self.all_high = True
             for output in self.outputs:
                 calls.append((LOW, self.name, output))
         else:
+            self.all_high = False
             for output in self.outputs:
                 calls.append((HIGH, self.name, output))
                 
@@ -132,52 +136,31 @@ def get_modules(lines):
                     modules[output].add_input(modules[name].name)
     return modules
 
-def star1(filename):
-    lines = get_input(filename)
-    
-    modules = get_modules(lines)
-    # modules["output"] = 
-    
-    broadcaster_outputs = []
-    for line in lines:
-        if line.startswith("broadcaster"):
-            broadcaster_outputs = line.split(" -> ")[1].split(", ")
-    
-    low_count = 0
-    high_count = 0
-    
-    for i in range(1000):
-        calls = deque()
-        # button -> broadcaster
-        low_count += 1
-        for output in broadcaster_outputs:
-            calls.append((LOW, "broadcaster", output))
-        
-        while calls:
-            pulse, source, destination = calls.popleft()
-            if pulse == LOW:
-                low_count += 1
-            else:
-                high_count += 1
-            # print(f"{source} {pulse} -> {destination}")
-            calls.extend(modules[destination].call(pulse, source))
-        
-    print(f"{high_count = }, {low_count = }")
-    
-    return high_count * low_count
-
 def star2(filename):
     lines = get_input(filename)
     
     modules = get_modules(lines)
     # modules["output"] = 
     
+    modules = get_modules(lines)
+    # modules["output"] = 
+    
     broadcaster_outputs = []
     for line in lines:
         if line.startswith("broadcaster"):
             broadcaster_outputs = line.split(" -> ")[1].split(", ")
     
-    for i in range(1000000000):
+    loops = {
+        # "xp": 0,
+        # "xl": 0,
+        # "gp": 0,
+        # "ln": 0,
+        "zp": 0,
+        "rg": 0,
+        "sj": 0,
+        "pp": 0,
+    }
+    for i in range(1000000000):  # button presses
         if i % 10000 == 0:
             print(f"{i = }")
         calls = deque()
@@ -187,9 +170,35 @@ def star2(filename):
         while calls:
             pulse, source, destination = calls.popleft()
             calls.extend(modules[destination].call(pulse, source))
-            if modules["rx"].called:
-                print(f"{i = }")
-                return i
+            if modules["zp"].all_high and loops["zp"] == 0:
+                print(f"zp all_high at {i = }")
+                loops["zp"] = i
+            if modules["rg"].all_high and loops["rg"] == 0: 
+                print(f"rg all_high at {i = }")
+                loops["rg"] = i
+            if modules["sj"].all_high and loops["sj"] == 0:
+                print(f"sj all_high at {i = }")
+                loops["sj"] = i
+            if modules["pp"].all_high and loops["pp"] == 0:
+                print(f"pp all_high at {i = }")
+                loops["pp"] = i
+
+            # if modules["xp"].all_high and loops["xp"] < 2:
+            #     print(f"xp all_high at {i = }")
+            #     loops["xp"] = i
+            # if modules["xl"].all_high and loops["xl"] < 2:
+            #     print(f"xl all_high at {i = }")
+            #     loops["xl"] = i
+            # if modules["gp"].all_high and loops["gp"] < 2:
+            #     print(f"gp all_high at {i = }")
+            #     loops["gp"] = i
+            # if modules["ln"].all_high and loops["ln"] < 2:
+            #     print(f"ln all_high at {i = }")
+            #     loops["ln"] = i
+            
+            if all(val > 1 for val in loops.values()):
+                # return loops["xp"] * loops["xl"] * loops["gp"] * loops["ln"]
+                return (loops["zp"], loops["rg"], loops["sj"], loops["pp"])
     
 
 def tests():
@@ -199,22 +208,51 @@ def tests():
 if __name__ == "__main__":
     tests()
 
-    ans = star1("example.txt")
-    print(f"example star 1: {ans}")
-    assert ans == 32000000, f"wrong answer: {ans}"
+    # ans = star1("example.txt")
+    # print(f"example star 1: {ans}")
+    # assert ans == 32000000, f"wrong answer: {ans}"
     
     # ans = star1("example2.txt")
     # print(f"example star 1: {ans}")
     # assert ans == 11687500, f"wrong answer: {ans}"
 
-    ans = star1("input.txt")
-    print(f"star 1: {ans}")
-    assert ans == 666795063
+    # ans = star1("input.txt")
+    # print(f"star 1: {ans}")
+    # assert ans == 666795063
 
     # ans = star2("example.txt")
     # print(f"example star 2: {ans}")
     # assert ans == 167409079868000
 
-    ans = star2("input.txt")
+    factors = star2("input.txt")
+    common = lcm(*factors)
+    print(f"{common = }")
+    
+    print([common/factor for factor in factors])
+    
+    ans = lcm(*[factor + 1 for factor in factors])
+    
+    
     print(f"star 2: {ans}")
-    # assert ans == 132186256794011
+    assert ans == 253302889093151
+    
+    # 253048940352000 too low
+    # 253048940352000 too low
+    # 253302889093151
+
+
+# rg all_high at i = 3832
+# pp all_high at i = 4020
+# zp all_high at i = 4050
+# sj all_high at i = 4056
+    
+# rg all_high at i = 7665
+# pp all_high at i = 8041
+# zp all_high at i = 8101
+# sj all_high at i = 8113
+
+
+# rg all_high at i = 11498
+# pp all_high at i = 12062
+# zp all_high at i = 12152
+# sj all_high at i = 12170

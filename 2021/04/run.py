d5c93ba4bffbdfbbe7d0f99f14682092d8ceccd0
Filip Sund <filip.sund@gmail.com>
2024-12-04 14:07:53 +0100

    2021 day 4 star 2

--- 2021/04/run.py
+++ 2021/04/run.py
@@ -66,14 +66,37 @@ def star1(filename):
     boards = get_boards(lines)
     
     for number in drawings:
-        print(number)
         for board in boards:
             mark_number(board, number)
             if check_if_winner(board):
                 s = get_sum_of_unmarked(board)
-                print(f"Winning number: {number}")
-                print(f"Winning board:\n{board}")
+                # print(f"Winning number: {number}")
+                # print(f"Winning board:\n{board}")
                 return s*number
+
+
+
+def star2(filename):
+    lines = get_input(filename)
+    drawings = lines[0]
+    drawings = [int(val) for val in drawings.split(",")]
+    boards = get_boards(lines)
+    
+    n_boards = len(boards)
+    winners = 0
+    for number in drawings:
+        for i in list(range(len(boards)))[::-1]:  # reverse order so we can pop
+            board = boards[i]
+            mark_number(board, number)
+            if check_if_winner(board):
+                winners += 1
+                if winners == n_boards:
+                    s = get_sum_of_unmarked(board)
+                    # print(f"Winning number: {number}")
+                    # print(f"Winning board:\n{board}")
+                    return s*number
+                else:
+                    boards.pop(i)
 
 
 def tests():
@@ -85,9 +108,9 @@ def tests():
     print(f"example star 1: {ans}")
     assert ans == 4512, f"wrong answer: {ans}"
     
-    # ans = star2("example.txt")
-    # print(f"example star 2: {ans}")
-    # assert ans == 9, f"wrong answer: {ans}"
+    ans = star2("example.txt")
+    print(f"example star 2: {ans}")
+    assert ans == 1924, f"wrong answer: {ans}"
 
 if __name__ == "__main__":
     tests()
@@ -96,6 +119,7 @@ if __name__ == "__main__":
     print(f"star 1: {ans}")
     assert ans == 11774
 
-    # ans = star2("input.txt")
-    # print(f"star 2: {ans}")
-    # assert ans == 1824
+    ans = star2("input.txt")
+    print(f"star 2: {ans}")
+    assert ans == 4495
+

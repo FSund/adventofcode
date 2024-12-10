7976ab5ddac797c20b1712779a12071e1a172090
Filip Sund <filip.sund@gmail.com>
2024-12-10 18:45:18 +0100

    2021 day 6 star 2

--- 2021/06/run.py
+++ 2021/06/run.py
@@ -1,5 +1,3 @@
-import math
-
 def get_input(filename):
     lines = []
     with open(filename) as file:
@@ -9,52 +7,45 @@ def get_input(filename):
     return lines
 
 
-def star1(filename, days=80):
+def aoc(filename, days=80):
     lines = get_input(filename)
     
     fish = [int(f) for f in lines[0].split(",")]
-    
-    first_days = days - 7*math.floor(days/7)
-    print(f"{first_days = }")
-    for day in range(first_days):
-        new_fish = []
-        for i in range(len(fish)):
-            if fish[i] == 0:
-                fish[i] = 6
-                new_fish.append(8)
-            else:
-                fish[i] -= 1
-
-        fish += new_fish
+    timers = [0 for i in range(9)]
+    for f in fish:
+        timers[f] += 1
         
-        # if day % 10 == 0:
-        #     print(len(fish))
-
-    n = len(fish)
-    print(f"{n = }")
-    exponent = int((days - first_days)/7) - 2
-    print(f"{exponent = }")
-    n = n * (2**exponent)
-
-    return n
+    for day in range(days):
+        n_spawned = timers[0]
+        timers[0] = timers[1]
+        timers[1] = timers[2]
+        timers[2] = timers[3]
+        timers[3] = timers[4]
+        timers[4] = timers[5]
+        timers[5] = timers[6]
+        timers[6] = timers[7] + n_spawned
+        timers[7] = timers[8]
+        timers[8] = n_spawned
+    
+    return sum(timers)
 
 
 def tests():
-    ans = star1("example.txt")
+    ans = aoc("example.txt", days=80)
     print(f"example star 1: {ans}")
     assert ans == 5934, f"wrong answer: {ans}"
     
-    ans = star1("example.txt", days=256)
+    ans = aoc("example.txt", days=256)
     print(f"example star 2: {ans}")
     assert ans == 26984457539, f"wrong answer: {ans}"
 
 if __name__ == "__main__":
     tests()
     
-    ans = star1("input.txt")
+    ans = aoc("input.txt", days=80)
     print(f"star 1: {ans}")
     assert ans == 387413
 
-    ans = star1("input.txt", days=256)
+    ans = aoc("input.txt", days=256)
     print(f"star 2: {ans}")
-    # assert ans == 23864
+    assert ans == 1738377086345

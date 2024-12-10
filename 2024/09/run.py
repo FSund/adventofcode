2e302d68e0dc756537b0ecbb5fdd5ff903b96f40
Filip Sund <filip.sund@gmail.com>
2024-12-10 09:36:47 +0100

    fix

--- 2024/09/run.py
+++ 2024/09/run.py
@@ -36,9 +36,9 @@ class FSObject:
                 self.blocks[i] = id
                 return True
         
-        return RuntimeError("No free block found")
+        raise RuntimeError("No free block found")
 
-def aoc(filename, star2=False):
+def star1(filename):
     lines = get_input(filename)
     line = lines[0]
     
@@ -54,7 +54,6 @@ def aoc(filename, star2=False):
             # free space
             fsobjects.append(FSObject(size, None))
 
-    n = len(fsobjects)
     assert fsobjects[1].blocks.count(None)
     assert fsobjects[-1].blocks.count(None) < len(fsobjects[-1].blocks)
     contigous = False
@@ -104,23 +103,93 @@ def aoc(filename, star2=False):
     return checksum
 
 
+def get_string_repr(fsobjects):
+    s = ""
+    for obj in fsobjects:
+        b = [str(b) for b in obj.blocks]
+        s += "".join(b)
+    s = s.replace("None", ".")
+    return s
+
+
+def star2(filename):
+    lines = get_input(filename)
+    line = lines[0]
+    
+    fsobjects = []
+    id = 0
+    for idx, size in enumerate(line):
+        size = int(size)
+        if idx % 2 == 0:
+            # file
+            fsobjects.append(FSObject(size, id))
+            id += 1
+        else:
+            # free space
+            fsobjects.append(FSObject(size, None))
+
+    assert fsobjects[1].blocks.count(None)
+    assert fsobjects[-1].blocks.count(None) < len(fsobjects[-1].blocks)
+    
+    # attempt to move each file once
+    for idx in reversed(range(0, len(fsobjects), 2)):
+        file_obj = fsobjects[idx]
+        
+        assert file_obj.is_full()
+        if file_obj.size == 0:
+            continue
+
+        # find first with enough free space
+        free_space_obj = None
+        for _, obj in enumerate(fsobjects[0:idx]):
+            if obj.blocks.count(None) >= len(file_obj.blocks):
+                free_space_obj = obj
+                break
+        
+        # if no free space found, skip moving this file
+        if not free_space_obj:
+            continue
+        
+        # move the whole file
+        for block_idx, block in enumerate(file_obj.blocks):
+            free_space_obj.insert_file_block(block)
+            file_obj.blocks[block_idx] = None
+
+    if len(fsobjects) < 20:
+        s = get_string_repr(fsobjects)
+        print(s)
+        assert s == "00992111777.44.333....5555.6666.....8888.."
+        
+
+    pos = 0
+    checksum = 0
+    for obj in fsobjects:
+        for block in obj.blocks:
+            if block is None:
+                block = 0
+            checksum += pos * block
+            pos += 1
+
+    return checksum
+
+
 def tests():
-    ans = aoc("example.txt")
+    ans = star1("example.txt")
     print(f"example star 1: {ans}")
     assert ans == 1928, f"wrong answer: {ans}"
     
-    # ans = aoc("example.txt", star2=True)
-    # print(f"example star 2: {ans}")
-    # assert ans == 34, f"wrong answer: {ans}"
+    ans = star2("example.txt")
+    print(f"example star 2: {ans}")
+    assert ans == 2858, f"wrong answer: {ans}"
 
 
 if __name__ == "__main__":
     tests()
     
-    ans = aoc("input.txt")
-    print(f"star 1: {ans}")
-    # assert ans == 271
+    # ans = star1("input.txt")
+    # print(f"star 1: {ans}")
+    # assert ans == 6353658451014
     
-    # ans = aoc("input.txt", star2=True)
-    # print(f"star 2: {ans}")
+    ans = star2("input.txt")
+    print(f"star 2: {ans}")
     # assert ans == 994

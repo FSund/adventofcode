23c752b931f81e98178d774ce7afc1c6c57b12d7
Filip Sund <fsund@deepoceangroup.com>
2025-12-08 13:28:42 +0100

    optimize

--- 2025/08/star2_disjointset.py
+++ 2025/08/star2_disjointset.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from scipy.cluster.hierarchy import DisjointSet
+import time
 
 
 def get_input(filename):
@@ -12,36 +13,43 @@ def get_input(filename):
     return lines
 
 
-def calc_dist(this, other):
-    x1, y1, z1 = [int(d) for d in this.split(",")]
-    x2, y2, z2 = [int(d) for d in other.split(",")]
-    return (x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2
-
-
 def aoc(filename):
     lines = get_input(filename)
 
-    # calculate distances
-    distance = []
+    # only parse strings once
+    boxes = []
+    for line in lines:
+        x, y, z = [int(d) for d in line.split(",")]
+        boxes.append([x, y, z])
+
+    t0 = time.perf_counter()
+
+    # Create all edges with distances
     n = len(lines)
+    edges = []
     for i in range(n):
+        box_i = boxes[i]
         for j in range(i+1, n):
-            d = calc_dist(lines[i], lines[j])
-            distance.append((d, (i,j)))
+            box_j = boxes[j]
+            
+            # inline distance calculation for speed
+            d = (box_i[0] - box_j[0])**2 + (box_i[1] - box_j[1])**2 + (box_i[2] - box_j[2])**2
+            edges.append((d, i, j))
     
-    distance = sorted(distance)
+    # sort by distance
+    edges.sort()
+
+    print(f"Build & sort: {time.perf_counter() - t0}")
+
     circuits = DisjointSet(list(range(n)))
-    for d in distance:
-        i = d[1][0]
-        j = d[1][1]
-        
+    for d, i, j in edges:
         # connect i to j
         circuits.merge(i, j)
         
         # only need to check a single circuit, since if all of them are connected, all of them are connected
         if circuits.subset_size(0) == n:
-            x1 = int(lines[i].split(",")[0])
-            x2 = int(lines[j].split(",")[0])
+            x1 = boxes[i][0]
+            x2 = boxes[j][0]
             return x1*x2
 
     raise RuntimeError()
@@ -55,7 +63,9 @@ def tests():
 
 if __name__ == "__main__":
     tests()
-    
+
+    t0= time.perf_counter()
     ans = aoc("input.txt")
     print(f"{ans = }")
+    print(f"Total time: {time.perf_counter() - t0}")
 
